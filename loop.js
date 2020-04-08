90177bfd6f1c46b5f8ff11f7b33e6d4a907c2e8a
Mini Davarasl <minidavarasl@gmail.com>
2020-04-08 14:04:39 +0200

    added loop exercise

---
+++ loop.js
@@ -0,0 +1,50 @@
+//count up to 10
+limit = 10
+
+for (let i = 0; i <= limit; i++) {
+    console.log(i)
+}
+
+// count down from 10 - my try :)
+
+limit = 10 
+
+for (let i = limit; i === 0; --i) {
+    console.log(i)
+}
+
+// count down from 10 - right answer
+
+limit = 10 
+
+for (let i = limit; i >= 0; i--) {
+    console.log(i)
+}
+
+// console log all even numbers - my try
+
+limit = 10
+
+for (x%2 === 0); i <= limit; i++) {
+    console.log(i)
+}
+
+// console log all even numbers - right answer
+
+limit = 10
+
+for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0 && i > 0) {
+    console.log (i)
+}
+
+for (let i = 0; i < i<=limit; i++) {
+    
+    const element = array[i];
+    
+}
+
+if (x%2 === 0) {console.log(' it is even' )}
+ it is even
+if (z%2 !== 0) {console.log(' it is odd' )} 
+  it is odd
