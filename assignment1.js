daacfd8d472675bd9e96ce999327d9f689983239
Mini Davarasl <minidavarasl@gmail.com>
2020-04-09 17:59:52 +0200

    added code in assignment1 js and html

--- assignment1.js
+++ assignment1.js
@@ -1,19 +1,17 @@
 var amount = prompt("Insert the value of your purchase ($):");
 var membership = prompt("Insert membership (None, Gold or Platinum?");
-// const discount = amount/100;
 
-let result = 0 + amount;
 
-if  (amount =< 100) {
-        console.log(result);
-        console.log("Your discount is " + result*0.05);
-        console.log("Your amount to pay is " + result*0.95);
-}   else if (amount =< 200) {
-    if (membership = input:"Gold");
+if  (amount => 100 < 199) {
+        console.log(amount);
+        console.log("Your discount is " + amount*0.05);
+        console.log("Your amount to pay is " + amount*0.95);
+}   else if (amount >= 200 < 299) {
+    if (membership = "Gold");
         console.log("Your discount is " + result*0.08);
         console.log("Your amount to pay is " + result*0.92);
-}   else if (amount =< 300) {
-    if (membership = input:"Platinum");
+}   else if (amount >= 200 < 299) {
+    if (membership = "Platinum");
     console.log("Your discount is " + result*0.12);
     console.log("Your amount to pay is " + result*0.88);
 }
